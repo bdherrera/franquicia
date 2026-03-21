1aabfe43a951b705161237ee3db8bed7743e4820
Brayan David Herrera Acero <braherrera2003@hotmail.com>
2026-03-21 00:15:54 -0500

    Initial commit

---
+++ src/main/java/co/prueba/accenture/backend/pruebaaccenturedev/PruebaAccentureDevApplication.java
@@ -0,0 +1,13 @@
+package co.prueba.accenture.backend.pruebaaccenturedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAccentureDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaAccentureDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/prueba/accenture/backend/pruebaaccenturedev/PruebaAccentureDevApplicationTests.java
@@ -0,0 +1,13 @@
+package co.prueba.accenture.backend.pruebaaccenturedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAccentureDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
